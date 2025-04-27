6b4a054014cf768178e7a5395ce45dac8ea595cd
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-04-27 19:08:08 +0200

    add basic number counter for histogram exercise

--- logic-exercises/histogram-generator/index.js
+++ logic-exercises/histogram-generator/index.js
@@ -1,1 +1,46 @@
+/*
+Escriba un programa en JavaScript que contenga un array con números enteros (del 1 al 5) que pueden estar repetidos 
+y genere en la consola (o en la terminal) un histograma que represente cuánto se repite cada número.
 
+Debe mostrar la frecuencia para todos los números, incluso si no están presentes en el array.
+Puede asumir que el array tendrá una longitud corta (hasta 20 números).
+
+Ejemplo:
+Entrada:
+const numeros = [1, 2, 1, 3, 3, 1, 2, 1, 5, 1];
+
+Salida esperada en consola:
+1: *****
+2: **
+3: **
+4:
+5: *
+*/
+
+const numbers = [1, 2, 1, 3, 3, 1, 2, 1, 5, 1];
+
+let countNumber1 = 0;
+let countNumber2 = 0;
+let countNumber3 = 0;
+let countNumber4 = 0;
+let countNumber5 = 0;
+
+for (const number of numbers) {
+  if (number === 1) {
+    countNumber1++;
+  } else if (number === 2) {
+    countNumber2++;
+  } else if (number === 3) {
+    countNumber3++;
+  } else if (number === 4) {
+    countNumber4++;
+  } else if (number === 5) {
+    countNumber5++;
+  }
+}
+
+console.log(`1: ${countNumber1}`);
+console.log(`2: ${countNumber2}`);
+console.log(`3: ${countNumber3}`);
+console.log(`4: ${countNumber4}`);
+console.log(`5: ${countNumber5}`);
